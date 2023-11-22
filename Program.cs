c593c5e3c941af5e0e2197f21930273e0b1a4bec
Dmitry <zimzulka1@gmail.com>
2023-11-23 00:43:51 +0400

    Добавил программу

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+string[] array= {"Hello", "2", "world", ":-)" };
+var result = new string [array.Length];
+int size = 0;
+foreach(var count in array)
+{
+   if(count.Length <=3)
+   {
+       result[size] = count;
+        size++;
+   }
+}
+Console.WriteLine(string.Join(Environment.NewLine,result,0,size));
